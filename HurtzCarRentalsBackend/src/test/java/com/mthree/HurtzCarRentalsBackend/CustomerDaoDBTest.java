5d8de392eb67ba64618bf25a7cb9a91023ba9c45
Joshua Seitz <jseitz2@luc.edu>
2021-01-11 13:18:17 -0600

    deleteCustomerById test

--- HurtzCarRentalsBackend/src/test/java/com/mthree/HurtzCarRentalsBackend/CustomerDaoDBTest.java
+++ HurtzCarRentalsBackend/src/test/java/com/mthree/HurtzCarRentalsBackend/CustomerDaoDBTest.java
@@ -8,18 +8,17 @@ package com.mthree.HurtzCarRentalsBackend;
 import com.mthree.HurtzCarRentalsBackend.entity.Customer;
 
 import com.mthree.HurtzCarRentalsBackend.dao.CustomerDao;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cglib.core.Local;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -68,6 +67,33 @@ public class CustomerDaoDBTest {
         
         Customer fromDao = customerDao.getCustomerById(customer.getCustomerId());
         assertEquals(customer, fromDao);
+    }
+    
+    @Test
+    public void testDeleteCustomerById() {
+        
+        Customer customer = new Customer();
+        customer.setFirstName("Test Customer First");
+        customer.setLastName("Test Customer Last");
+        
+        Calendar cal = Calendar.getInstance();
+        cal.set(1985,1,8);
+        customer.setDob(cal.getTime());
+        
+        customer.setLicenseNumber("S-001-2033");
+        customer.setLoyaltyPts(0);
+                
+        customer = customerDao.addCustomer(customer);
+        
+        Customer fromDao = customerDao.getCustomerById(customer.getCustomerId());
+        assertEquals(customer, fromDao);
+        
+        customerDao.deleteCustomerById(customer.getCustomerId());
+        
+        fromDao = customerDao.getCustomerById(customer.getCustomerId());
+        assertNull(fromDao);
+        
+        
     }
     
 }
