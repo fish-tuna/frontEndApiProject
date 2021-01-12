e11f8101fedb6a6e2394b91e541eeea973b0ecc6
Joshua Seitz <jseitz2@luc.edu>
2021-01-12 10:06:54 -0600

    testAddReservation

--- HurtzCarRentalsBackend/src/test/java/com/mthree/HurtzCarRentalsBackend/ReservationDaoDBTest.java
+++ HurtzCarRentalsBackend/src/test/java/com/mthree/HurtzCarRentalsBackend/ReservationDaoDBTest.java
@@ -5,7 +5,14 @@
  */
 package com.mthree.HurtzCarRentalsBackend;
 
+import com.mthree.HurtzCarRentalsBackend.dao.VehicleDao;
+import com.mthree.HurtzCarRentalsBackend.dao.ReservationDao;
+import com.mthree.HurtzCarRentalsBackend.entity.Reservation;
+import java.util.Calendar;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -17,5 +24,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ReservationDaoDBTest {
+    
+    @Autowired
+    ReservationDao reservationDao;
+    
+    @Test
+    public void testAddReservation() {
+        Reservation reservation = new Reservation();
+        reservation.setBeforeTax(1.00);
+        reservation.setCustomerLicenseNumber("S239581");
+        reservation.setDiscount(0.00);
+        reservation.setReservationId(294);
+        reservation.setTax(3.00);
+        
+        Calendar startDate = Calendar.getInstance();
+        startDate.set(2021,2,1);
+        reservation.setStartDate(startDate.getTime());
+        
+        Calendar endDate = Calendar.getInstance();
+        endDate.set(2021,2,5);
+        reservation.setEndDate(endDate.getTime());
+        
+        reservationDao.addReservation(reservation);
+        
+        Reservation fromDao = reservationDao.getReservationById(reservation.getReservationId());
+        assertEquals(reservation, fromDao);
+    }
     
 }
