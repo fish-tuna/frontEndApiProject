7bc52314169019f3c74266b96f7f0a1e8314cc2f
fish-tuna <56567684+fish-tuna@users.noreply.github.com>
2021-01-13 11:33:55 -0500

    finish search and edit functionalities

--- Frontend/reactcomponents1/src/Edit.js
+++ Frontend/reactcomponents1/src/Edit.js
@@ -1,8 +1,6 @@
 import { useState, useRef } from "react";
 import { Form, Button } from "semantic-ui-react";
 import ConditionalBanner from "./ConditionalBanner";
-//need to implement one of two things: go back to updated search results upon successful edit/delete,
-//or go back to search homepage after successful edit/delete. The latter would take much less time to implement.
 function Edit(props) {
   //conditional validation error display
   const [banner, toggleBanner] = useState(false);
@@ -14,6 +12,25 @@ function Edit(props) {
   const firstName = useRef(null);
   const lastName = useRef(null);
   const loyaltyPoints = useRef(null);
+  //inventory/vehicle refs
+  const licensePlate = useRef(null);
+  const make = useRef(null);
+  const model = useRef(null);
+  const category = useRef(null);
+  //reservation refs
+  const reservationID = useRef(null);
+  const resCustomerID = useRef(null);
+  const resLicensePlate = useRef(null);
+  const startDate = useRef(null);
+  const endDate = useRef(null);
+  const beforeTax = useRef(null);
+  const tax = useRef(null);
+  const discount = useRef(null);
+  const totalPrice = useRef(null);
+  //edit and delete methods below
+  //could add functionality to prevent edit submission if nothing's changed
+  //could separate banner state into one for each window
+  //customer edit and delete
   const handleCustomerEdit = (e) => {
     if (
       !(
@@ -27,7 +44,7 @@ function Edit(props) {
       toggleBanner(true);
     } else {
       toggleBanner(false);
-      fetch("http://localhost:8080/customers/{" + props.data.customerID + "}", {
+      fetch("http://localhost:8080/customers/{" + props.data.customerId + "}", {
         method: "PUT",
         body: JSON.stringify({
           customerId: customerID.current.value,
@@ -38,9 +55,10 @@ function Edit(props) {
         }),
       })
         .then((res) => res.json())
-        //!!! implement error handling
+        //implement error handling?
         .then((result) => {
-          /*go back*/
+          props.editResults(props.type, result);
+          props.goBack(false);
         })
         .catch((err) => {
           console.log("error");
@@ -50,13 +68,136 @@ function Edit(props) {
   };
   const handleCustomerDelete = () => {
     toggleBanner(false);
-    fetch("http://localhost:8080/customers/{" + props.data.customerID + "}", {
+    fetch("http://localhost:8080/customers/{" + props.data.customerId + "}", {
       method: "DELETE",
     })
       .then((res) => res.json())
-      //!!! implement error handling. display banner if there is error, return to search results and update if no error
+      //implement error handling? display banner if there is error, return to search results and update if no error
       .then((result) => {
-        /*go back*/
+        props.deleteResults(props.type, result.customerId);
+        props.goBack(false);
+      })
+      .catch((err) => {
+        console.log("error");
+        toggleErrorBanner(true);
+      });
+  };
+  //inventory edit and delete
+  const handleInventoryEdit = (e) => {
+    if (
+      !(
+        licensePlate.current.value ||
+        category.current.value ||
+        make.current.value ||
+        model.current.value
+      )
+    ) {
+      e.preventDefault();
+      toggleBanner(true);
+    } else {
+      toggleBanner(false);
+      fetch(
+        "http://localhost:8080/vehicles/{" + props.data.licensePlate + "}",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            licensePlate: licensePlate.current.value,
+            category: category.current.value,
+            model: model.current.value,
+            make: make.current.value,
+            //below needed?
+            //color: color.current.value
+          }),
+        }
+      )
+        .then((res) => res.json())
+        //implement error handling?
+        .then((result) => {
+          props.editResults(props.type, result);
+          props.goBack(false);
+        })
+        .catch((err) => {
+          console.log("error");
+          toggleErrorBanner(true);
+        });
+    }
+  };
+  const handleInventoryDelete = () => {
+    toggleBanner(false);
+    fetch("http://localhost:8080/vehicles/{" + props.data.licensePlate + "}", {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      //implement error handling? display banner if there is error, return to search results and update if no error
+      .then((result) => {
+        props.deleteResults(props.type, result.customerId);
+        props.goBack(false);
+      })
+      .catch((err) => {
+        console.log("error");
+        toggleErrorBanner(true);
+      });
+  };
+  //reservation edit and delete
+  const handleReservationEdit = (e) => {
+    if (
+      !(
+        resCustomerID.current.value ||
+        resLicensePlate.current.value ||
+        startDate.current.value ||
+        endDate.current.value ||
+        beforeTax.current.value ||
+        tax.current.value ||
+        discount.current.value ||
+        totalPrice.current.value
+      )
+    ) {
+      e.preventDefault();
+      toggleBanner(true);
+    } else {
+      toggleBanner(false);
+      fetch(
+        "http://localhost:8080/reservations/{" + props.data.reservationId + "}",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            reservationId: reservationID.current.value,
+            customerId: resCustomerID.current.value,
+            licensePlate: resLicensePlate.current.value,
+            startDate: startDate.current.value,
+            endDate: endDate.current.value,
+            beforeTax: beforeTax.current.value,
+            tax: tax.current.value,
+            discount: discount.current.value,
+            totalPrice: totalPrice.current.value,
+          }),
+        }
+      )
+        .then((res) => res.json())
+        //implement error handling?
+        .then((result) => {
+          props.editResults(props.type, result);
+          props.goBack(false);
+        })
+        .catch((err) => {
+          console.log("error");
+          toggleErrorBanner(true);
+        });
+    }
+  };
+  const handleReservationDelete = () => {
+    toggleBanner(false);
+    fetch(
+      "http://localhost:8080/reservations/{" + props.data.reservationId + "}",
+      {
+        method: "DELETE",
+      }
+    )
+      .then((res) => res.json())
+      //implement error handling? display banner if there is error, return to search results and update if no error
+      .then((result) => {
+        props.deleteResults(props.type, result.customerId);
+        props.goBack(false);
       })
       .catch((err) => {
         console.log("error");
@@ -114,6 +255,116 @@ function Edit(props) {
           Submit
         </Button>
         <Button onClick={handleCustomerDelete}>Delete</Button>
+      </Form>
+    </div>;
+  } else if (props.type === "inventory") {
+    <div>
+      <ConditionalBanner displayBool={banner} type={"field"} />
+      <ConditionalBanner displayBool={errorBanner} type={"error"} />
+      <Form>
+        <Form.Field>
+          <label>License Plate</label>
+          <input
+            ref={licensePlate}
+            placeholder="License Number"
+            value={props.data.licensePlate}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Category</label>
+          <input
+            ref={category}
+            placeholder="Category"
+            value={props.data.category}
+            readOnly
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Make</label>
+          <input ref={make} placeholder="Make" value={props.data.make} />
+        </Form.Field>
+        <Form.Field>
+          <label>Model</label>
+          <input ref={model} placeholder="Model" value={props.data.model} />
+        </Form.Field>
+
+        <Button type="submit" onClick={handleInventoryEdit}>
+          Submit
+        </Button>
+        <Button onClick={handleInventoryDelete}>Delete</Button>
+      </Form>
+    </div>;
+  } else if (props.type === "reservation") {
+    <div>
+      <ConditionalBanner displayBool={banner} type={"field"} />
+      <ConditionalBanner displayBool={errorBanner} type={"error"} />
+      <Form>
+        <Form.Field>
+          <label>Reservation ID</label>
+          <input
+            ref={reservationID}
+            placeholder="Reservation ID"
+            value={props.data.reservationId}
+            readOnly
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Customer ID</label>
+          <input
+            ref={resCustomerID}
+            placeholder="Customer ID"
+            value={props.data.customerId}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Start Date</label>
+          <input
+            ref={startDate}
+            placeholder="Start Date"
+            value={props.data.startDate}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>End Date</label>
+          <input
+            ref={endDate}
+            placeholder="End Date"
+            value={props.data.endDate}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Base Price</label>
+          <input
+            ref={beforeTax}
+            placeholder="Base Price"
+            value={props.data.beforeTax}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Tax</label>
+          <input ref={tax} placeholder="Tax" value={props.data.tax} />
+        </Form.Field>
+        <Form.Field>
+          <label>Discount</label>
+          <input
+            ref={discount}
+            placeholder="Discount"
+            value={props.data.discount}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Total Price</label>
+          <input
+            ref={totalPrice}
+            placeholder="Total Price"
+            value={props.data.totalPrice}
+          />
+        </Form.Field>
+
+        <Button type="submit" onClick={handleReservationEdit}>
+          Submit
+        </Button>
+        <Button onClick={handleReservationDelete}>Delete</Button>
       </Form>
     </div>;
   }
--- Frontend/reactcomponents1/src/SearchResults.js
+++ Frontend/reactcomponents1/src/SearchResults.js
@@ -3,8 +3,47 @@ import { Table, Button } from "semantic-ui-react";
 import Edit from "./Edit";
 
 function SearchResults(props) {
+  const [searchResults, setSearchResults] = useState(props.searchResults);
   const [displayEditPane, toggleDisplayEditPane] = useState(false);
   const [selectedData, setSelectedData] = useState(null);
+
+  //edit search results (function to be passed to Edit component)
+  const editResults = (type, editedResult) => {
+    if (type === "customer") {
+      setSearchResults(
+        searchResults.map(
+          (i) =>
+            (i = i.customerId === editedResult.customerId ? editedResult : i)
+        )
+      );
+    } else if (type === "inventory") {
+      setSearchResults(
+        searchResults.map(
+          (i) =>
+            (i =
+              i.licensePlate === editedResult.licensePlate ? editedResult : i)
+        )
+      );
+    } else if (type === "reservation") {
+      setSearchResults(
+        searchResults.map(
+          (i) =>
+            (i =
+              i.reservationId === editedResult.reservationId ? editedResult : i)
+        )
+      );
+    }
+  };
+  //delete search results (function to be passed to Edit component)
+  const deleteResults = (type, id) => {
+    if (type === "customer") {
+      setSearchResults(searchResults.filter((i) => i.customerId !== id));
+    } else if (type === "inventory") {
+      setSearchResults(searchResults.filter((i) => i.licensePlate !== id));
+    } else if (type === "reservation") {
+      setSearchResults(searchResults.filter((i) => i.reservationId !== id));
+    }
+  };
   //handle empty results, this conditional probably needs to be modified to fit error message
   if (!props.searchResults) {
     return (
@@ -15,7 +54,13 @@ function SearchResults(props) {
   } else if (displayEditPane) {
     return (
       <div>
-        <Edit type={props.type} data={selectedData} />
+        <Edit
+          type={props.type}
+          data={selectedData}
+          editResults={editResults}
+          deleteResults={deleteResults}
+          goBack={toggleDisplayEditPane}
+        />
       </div>
     );
   } else if (props.type === "customer") {
@@ -34,7 +79,7 @@ function SearchResults(props) {
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {props.searchResults.map((result) => (
+            {searchResults.map((result) => (
               <Table.Row key={result.customerId}>
                 <Table.Cell>{result.customerId}</Table.Cell>
                 <Table.Cell>{result.firstName}</Table.Cell>
@@ -64,23 +109,29 @@ function SearchResults(props) {
         <Table celled padded>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell singleLine>Customer ID</Table.HeaderCell>
-              <Table.HeaderCell>First Name</Table.HeaderCell>
-              <Table.HeaderCell>Last Name</Table.HeaderCell>
-              <Table.HeaderCell>Date of Birth</Table.HeaderCell>
-              <Table.HeaderCell>License Number</Table.HeaderCell>
-              <Table.HeaderCell>Loyalty Points</Table.HeaderCell>
+              <Table.HeaderCell singleLine>License Plate</Table.HeaderCell>
+              <Table.HeaderCell>Category</Table.HeaderCell>
+              <Table.HeaderCell>Model</Table.HeaderCell>
+              <Table.HeaderCell>Make</Table.HeaderCell>
+              <Table.HeaderCell>{/*empty*/}</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {props.searchResults.map((result) => (
-              <Table.Row key={result.customerId}>
-                <Table.Cell>{result.customerId}</Table.Cell>
-                <Table.Cell>{result.firstName}</Table.Cell>
-                <Table.Cell>{result.lastName}</Table.Cell>
-                <Table.Cell>{result.dateOfBirth}</Table.Cell>
-                <Table.Cell>{result.licenseNumber}</Table.Cell>
-                <Table.Cell>{result.loyaltyPoints}</Table.Cell>
+            {searchResults.map((result) => (
+              <Table.Row key={result.licensePlate}>
+                <Table.Cell>{result.licensePlate}</Table.Cell>
+                <Table.Cell>{result.category}</Table.Cell>
+                <Table.Cell>{result.make}</Table.Cell>
+                <Table.Cell>{result.model}</Table.Cell>
+
+                <Table.Cell>
+                  <Button
+                    onClick={() => {
+                      setSelectedData(result);
+                      toggleDisplayEditPane(true);
+                    }}
+                  ></Button>
+                </Table.Cell>
               </Table.Row>
             ))}
           </Table.Body>
@@ -95,23 +146,38 @@ function SearchResults(props) {
         <Table celled padded>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell singleLine>Customer ID</Table.HeaderCell>
-              <Table.HeaderCell>First Name</Table.HeaderCell>
-              <Table.HeaderCell>Last Name</Table.HeaderCell>
-              <Table.HeaderCell>Date of Birth</Table.HeaderCell>
-              <Table.HeaderCell>License Number</Table.HeaderCell>
-              <Table.HeaderCell>Loyalty Points</Table.HeaderCell>
+              <Table.HeaderCell singleLine>Reservation ID</Table.HeaderCell>
+              <Table.HeaderCell>Customer ID</Table.HeaderCell>
+              <Table.HeaderCell>License Plate</Table.HeaderCell>
+              <Table.HeaderCell>Start Date</Table.HeaderCell>
+              <Table.HeaderCell>End Date</Table.HeaderCell>
+              <Table.HeaderCell>Base Price</Table.HeaderCell>
+              <Table.HeaderCell>Tax</Table.HeaderCell>
+              <Table.HeaderCell>Discount</Table.HeaderCell>
+              <Table.HeaderCell>Total Price</Table.HeaderCell>
+              <Table.HeaderCell>{/*empty*/}</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {props.searchResults.map((result) => (
-              <Table.Row key={result.customerId}>
+            {searchResults.map((result) => (
+              <Table.Row key={result.reservationId}>
+                <Table.Cell>{result.reservationId}</Table.Cell>
                 <Table.Cell>{result.customerId}</Table.Cell>
-                <Table.Cell>{result.firstName}</Table.Cell>
-                <Table.Cell>{result.lastName}</Table.Cell>
-                <Table.Cell>{result.dateOfBirth}</Table.Cell>
-                <Table.Cell>{result.licenseNumber}</Table.Cell>
-                <Table.Cell>{result.loyaltyPoints}</Table.Cell>
+                <Table.Cell>{result.licensePlate}</Table.Cell>
+                <Table.Cell>{result.startDate}</Table.Cell>
+                <Table.Cell>{result.endDate}</Table.Cell>
+                <Table.Cell>{result.beforeTax}</Table.Cell>
+                <Table.Cell>{result.tax}</Table.Cell>
+                <Table.Cell>{result.discount}</Table.Cell>
+                <Table.Cell>{result.totalPrice}</Table.Cell>
+                <Table.Cell>
+                  <Button
+                    onClick={() => {
+                      setSelectedData(result);
+                      toggleDisplayEditPane(true);
+                    }}
+                  ></Button>
+                </Table.Cell>
               </Table.Row>
             ))}
           </Table.Body>
